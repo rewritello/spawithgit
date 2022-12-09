918257af807e1af1625bd9e38ba89dd69137a30e
Ruslan <ruslanrttd@gmail.com>
2022-12-09 14:29:29 +0300

    Создан и написан класс User

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spa
+{
+    static class User
+    {
+        public static string login { get; set; }
+        public static string password;
+        public static string fio;
+        public static string pn;
+        public static string adress;
+    }
+}
