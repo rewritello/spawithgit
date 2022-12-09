457ef2245adaa56a9fef407c4fc81ebb4e179637
Ruslan <ruslanrttd@gmail.com>
2022-12-09 14:39:06 +0300

    Создана и написана форма StartForm

---
+++ StartForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace spa
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AutorisationButton = new System.Windows.Forms.Button();
+            this.RegistrationButton = new System.Windows.Forms.Button();
+            this.CheckCatalogButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AutorisationButton
+            // 
+            this.AutorisationButton.Location = new System.Drawing.Point(203, 93);
+            this.AutorisationButton.Name = "AutorisationButton";
+            this.AutorisationButton.Size = new System.Drawing.Size(372, 89);
+            this.AutorisationButton.TabIndex = 0;
+            this.AutorisationButton.Text = "Авторизироваться";
+            this.AutorisationButton.UseVisualStyleBackColor = true;
+            this.AutorisationButton.Click += new System.EventHandler(this.AutorisationButton_Click);
+            // 
+            // RegistrationButton
+            // 
+            this.RegistrationButton.Location = new System.Drawing.Point(202, 208);
+            this.RegistrationButton.Name = "RegistrationButton";
+            this.RegistrationButton.Size = new System.Drawing.Size(372, 83);
+            this.RegistrationButton.TabIndex = 1;
+            this.RegistrationButton.Text = "Зарегистрироваться";
+            this.RegistrationButton.UseVisualStyleBackColor = true;
+            this.RegistrationButton.Click += new System.EventHandler(this.RegistrationButton_Click);
+            // 
+            // CheckCatalogButton
+            // 
+            this.CheckCatalogButton.Location = new System.Drawing.Point(203, 323);
+            this.CheckCatalogButton.Name = "CheckCatalogButton";
+            this.CheckCatalogButton.Size = new System.Drawing.Size(370, 89);
+            this.CheckCatalogButton.TabIndex = 2;
+            this.CheckCatalogButton.Text = "Просмотреть каталог";
+            this.CheckCatalogButton.UseVisualStyleBackColor = true;
+            this.CheckCatalogButton.Click += new System.EventHandler(this.CheckCatalogButton_Click);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckCatalogButton);
+            this.Controls.Add(this.RegistrationButton);
+            this.Controls.Add(this.AutorisationButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SPA";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AutorisationButton;
+        private System.Windows.Forms.Button RegistrationButton;
+        private System.Windows.Forms.Button CheckCatalogButton;
+    }
+}
---
+++ StartForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spa
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AutorisationButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AutorisationForm autorisationForm = new AutorisationForm();
+            autorisationForm.Show();
+        }
+
+        private void RegistrationButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegistrationForm registrationForm = new RegistrationForm();
+            registrationForm.Show();
+        }
+
+        private void CheckCatalogButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CatalogForNonregisteredUserForm catalogForNonregisteredUser = new CatalogForNonregisteredUserForm();
+            catalogForNonregisteredUser.Show();
+        }
+    }
+}
